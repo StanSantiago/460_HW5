0b511c8ab98c22bd101736522b089044f3f42a85
AnEntireSkeleton <AnEntireSkeleton@users.noreply.github.com>
2018-10-28 17:18:09 -0700

    Initial commit; created base page layout

---
+++ MVC_Simple_DB/Controllers/ReqFormController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Simple_DB.Controllers
+{
+    public class ReqFormController : Controller
+    {
+        // GET: Req
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Simple_DB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Simple_DB.MvcApplication" Language="C#" %>
